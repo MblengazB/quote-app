47c3fc8e87e61a8fb0220299fcd520593f5d5e4d
AndrewIndeche <indecheandrew@gmail.com>
2021-08-20 21:00:18 +0300

    Add title class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-     Quote='Whatever Your Mind Can Conceive and Believe, It Can Achieve.'
+     title='Welcome to My Quote Hub'
+     Quote=(1,'Whatever Your Mind Can Conceive and Believe, It Can Achieve.','Napoleon hill')
+
+
 
   constructor() { }
 
