2905422a29c2ad24f2f329d9db0017b513918163
AndrewIndeche <indecheandrew@gmail.com>
2021-08-20 20:54:51 +0300

    Add quotes array class and description

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+     Quote='Whatever Your Mind Can Conceive and Believe, It Can Achieve.'
 
   constructor() { }
 
