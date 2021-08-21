a8177e5573c740fc2398d5548b7f29b549629597
AndrewIndeche <indecheandrew@gmail.com>
2021-08-21 17:15:28 +0300

    Add form class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,16 +1,5 @@
 export class Quote {
-  id:number;
-  name:string;
-  title:string;
-  quote:string;
-  author:string;
-
-  constructor(id:number,name:string,title:string,quote:string,author:string){
-      this.id=id;
-      this.name=name;
-      this.title=title;
-      this.quote=quote;
-      this.author=author;
+  constructor(public id:number,public name:string,public title:string,public description:string,public author:string){
 
   }
 
