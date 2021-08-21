bb7b9210cabbdb743aeb34c110808a5d47de25ec
AndrewIndeche <indecheandrew@gmail.com>
2021-08-21 11:43:07 +0300

    Display ngIF array

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,17 @@
 export class Quote {
-  id:number
-  Quote:string
+  id:number;
+  name:string;
+  title:string;
+  quote:string;
+  author:string;
+
+  constructor(id:number,name:string,title:string,quote:string,author:string){
+      this.id=id;
+      this.name=name;
+      this.title=title;
+      this.quote=quote;
+      this.author=author;
+
+  }
+
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  myQuotes:Quote []=[
-     title='Welcome to My Quote Hub'
-     Quote (1,'Motivational Quote','Whatever your mind can conceive and believe, it can achieve','Napoleon Hill')
-     Quote(2,'Life Quote','A coward dies a thousand deaths A soldier dies but once','Tupac Shakur')
-     Quote(3,'celebrity glitz quote','I dont think i just walk','Paris Hilton')
-     Quote(4,'Business Quote','Always deliver more than expected','larry Page')
-     Quote(5,'Sports Quote','You miss 100% of the shots you do not take','wayne Gretzky')
-     Quote(6,'Comedy Quote','my cell phone fell off my lap.I was reaching for it in my back','Eddie Griffin')
+  title='Welcome to My Quote Hub'
+  quotes:Quote []=[
+    new Quote (1,'Peter','Motivational quote','Whatever your mind can conceive and believe it can achieve','Napoleon Hill'),
+     new Quote(2,'Paul','Life quote','A coward dies a thousand deaths A soldier dies but once','Tupac Shakur'),
+     new Quote(3,'Mark','celebrity glitz quote','I dont think i just walk','Paris Hilton'),
+     new Quote(4,'Onyango','Business quote','Always deliver more than expected','larry Page'),
+     new Quote(5,'Dequan','Sports quote','You miss 100% of the shots you do not take','wayne Gretzky'),
+     new Quote(6,'Gustavo','Comedy quote','People say nothing is impossible but i do nothing everyday','A Milne'),
 ];
   constructor() { }
 
