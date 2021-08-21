ce602d4e9f1ce7f36d0768109e66f6f8ce9b00f8
AndrewIndeche <indecheandrew@gmail.com>
2021-08-21 21:20:05 +0300

    Add proporty binding logic

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
+  showDescription:boolean;
   constructor(public id:number,public name:string,public title:string,public description:string,public author:string){
-
+    this.showDescription=false;
   }
 
 }
