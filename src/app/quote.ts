e4f97faa6759352d9c113e20de6e399c7c382508
AndrewIndeche <indecheandrew@gmail.com>
2021-08-22 15:33:41 +0300

    Add two way data binding

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-  showdescription:boolean;
-  constructor(public id:number,public name:string,public title:string,public description:string,public author:string){
-    this.showdescription=false;
+
+  constructor(id:number,public name:string,public title:string,public description:string,public author:string,){
+  
   }
 }
